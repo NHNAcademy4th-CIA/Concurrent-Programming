44f8de7bcb2f70d4e68f909e15138f73763e0afe
LeeJungBum <jungbum05114@gmail.com>
2023-10-12 00:28:05 +0900

    feat: Quiz1-4

---
+++ src/main/java/org/nhnacademy/leejungbum/counter/counter4/TestCounter.java
@@ -0,0 +1,12 @@
+package org.nhnacademy.leejungbum.counter.counter4;
+
+public class TestCounter {
+    public static void main(String[] args) {
+        CounterThread counterThread1 = new CounterThread("CounterThread1");
+        CounterThread counterThread2 = new CounterThread("CounterThread2");
+
+        counterThread1.start();
+        counterThread2.start();
+    }
+}
+
