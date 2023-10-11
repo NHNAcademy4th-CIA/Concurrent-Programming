0c4ba907cd079325aae47859bdc3de9581f8c3db
LeeJungBum <jungbum05114@gmail.com>
2023-10-12 00:40:18 +0900

    refactor: 좀더 이해하기쉬운예제로 변경

--- src/main/java/org/nhnacademy/leejungbum/runnable/runnable1/TestCounter.java
+++ src/main/java/org/nhnacademy/leejungbum/runnable/runnable1/TestCounter.java
@@ -4,9 +4,13 @@ import org.nhnacademy.leejungbum.counter.counter2.Counter;
 
 public class TestCounter {
     public static void main(String[] args) {
-        CounterRunnable counterRunnable = new CounterRunnable("CounterRunnable");
-        Thread thread = new Thread(counterRunnable);
+        CounterRunnable counterRunnable1 = new CounterRunnable("CounterRunnable1");
+        CounterRunnable counterRunnable2 = new CounterRunnable("CounterRunnable2");
 
+        Thread thread = new Thread(counterRunnable1);
+
+//        counterRunnable1.run();
         thread.start();
+        counterRunnable2.run();
     }
 }
