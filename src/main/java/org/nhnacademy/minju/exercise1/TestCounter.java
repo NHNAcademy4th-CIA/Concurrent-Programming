dc1f601ddafc725440ca58b9a9e8c9323b643b14
minm063 <jangmin063@gmail.com>
2023-10-11 21:47:01 -0700

    feat: exercise1-4

--- src/main/java/org/nhnacademy/minju/exercise1/TestCounter.java
+++ src/main/java/org/nhnacademy/minju/exercise1/TestCounter.java
@@ -2,7 +2,10 @@ package org.nhnacademy.minju.exercise1;
 
 public class TestCounter {
     public static void main(String[] args) {
-        CounterThread counterThread = new CounterThread("CounterThread");
-        counterThread.start();
+        CounterThread counterThread1 = new CounterThread("CounterThread1");
+        CounterThread counterThread2 = new CounterThread("CounterThread2");
+
+        counterThread1.start();
+        counterThread2.start();
     }
 }
