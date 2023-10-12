adbb66eb208595d9750cfbb848126e31534329db
minm063 <jangmin063@gmail.com>
2023-10-12 00:09:05 -0700

    feat: exercise2-3

--- src/main/java/org/nhnacademy/minju/exercise2/CounterRunnable.java
+++ src/main/java/org/nhnacademy/minju/exercise2/CounterRunnable.java
@@ -3,20 +3,32 @@ package org.nhnacademy.minju.exercise2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CounterRunnable implements Runnable{
+public class CounterRunnable implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(CounterRunnable.class);
+    private Thread thread;
     private String name;
     private int count;
 
     public CounterRunnable(String name) {
         this.name = name;
         this.count = 0;
+        thread = new Thread(this, name);
+    }
+
+    public void start() {
+        thread.start();
     }
 
     @Override
     public void run() {
         while (!Thread.interrupted()) {
-            logger.info("{} : {}", name, ++count);
+            try {
+                ++count;
+                System.out.println(name + " : " + count);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
--- src/main/java/org/nhnacademy/minju/exercise2/TestCounter.java
+++ src/main/java/org/nhnacademy/minju/exercise2/TestCounter.java
@@ -3,11 +3,9 @@ package org.nhnacademy.minju.exercise2;
 public class TestCounter {
     public static void main(String[] args) {
         CounterRunnable counterRunnable1 = new CounterRunnable("CounterRunnable1");
-        Thread thread1 = new Thread(counterRunnable1);
         CounterRunnable counterRunnable2 = new CounterRunnable("CounterRunnable2");
-        Thread thread2 = new Thread(counterRunnable2);
 
-        thread1.start();
-        thread2.start();
+        counterRunnable1.start();
+        counterRunnable2.start();
     }
 }
