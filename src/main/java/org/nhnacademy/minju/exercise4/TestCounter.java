86a65e7b0261407e7c065aff5f771d915954c586
minm063 <jangmin063@gmail.com>
2023-10-12 10:58:44 -0700

    feat: exercise 5-3

--- src/main/java/org/nhnacademy/minju/exercise4/TestCounter.java
+++ src/main/java/org/nhnacademy/minju/exercise4/TestCounter.java
@@ -3,7 +3,9 @@ package org.nhnacademy.minju.exercise4;
 public class TestCounter {
     public static void main(String[] args) {
         Counter counter = new Counter();
-        int numberOfIterations = 10000;
+        int numberOfIterations = 100_000000;
+
+        long start = System.nanoTime();
 
         CountAgent agent1 = new CountAgent(counter, numberOfIterations, true);
         CountAgent agent2 = new CountAgent(counter, numberOfIterations, false);
@@ -16,9 +18,9 @@ public class TestCounter {
             agent2.join();
         } catch (InterruptedException ignore) {
         }
-
+        long end = System.nanoTime();
         System.out.println("Count : " + counter.getCount());
         System.out.println("Another Count : " + counter.getAnotherCount());
-
+        System.out.println("실행 시간 : " + (end - start) / 1000000000 + "초");
     }
 }
