37ed3daabd73ec153d81417c23fa360526e5648f
minm063 <jangmin063@gmail.com>
2023-10-12 02:59:57 -0700

    feat: exercise5-1

---
+++ src/main/java/org/nhnacademy/minju/exercise4/Counter.java
@@ -0,0 +1,17 @@
+package org.nhnacademy.minju.exercise4;
+
+public class Counter {
+    long count;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public long getCount() {
+        return count;
+    }
+}
---
+++ src/main/java/org/nhnacademy/minju/exercise4/TestCounter.java
@@ -0,0 +1,23 @@
+package org.nhnacademy.minju.exercise4;
+
+public class TestCounter {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        int numberOfIterations = 10000;
+
+        CountAgent agent1 = new CountAgent(counter, numberOfIterations, true);
+        CountAgent agent2 = new CountAgent(counter, numberOfIterations, false);
+
+        agent1.start();
+        agent2.start();
+
+        try {
+            agent1.join();
+            agent2.join();
+        } catch (InterruptedException ignore) {
+        }
+
+        System.out.println("Count : " + counter.getCount());
+
+    }
+}
