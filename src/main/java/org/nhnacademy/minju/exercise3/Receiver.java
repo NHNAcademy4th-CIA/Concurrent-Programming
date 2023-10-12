7345f18109ceaabbb028d49fcbbfca030f2308d6
minm063 <jangmin063@gmail.com>
2023-10-12 10:29:37 -0700

    feat: exercise4-1

---
+++ src/main/java/org/nhnacademy/minju/exercise3/Receiver.java
@@ -0,0 +1,29 @@
+package org.nhnacademy.minju.exercise3;
+
+import java.time.LocalDateTime;
+
+public class Receiver implements Runnable{
+    final Pipe pipe;
+    int endData;
+
+    public Receiver(Pipe pipe, int endData) {
+        this.pipe = pipe;
+        this.endData = endData;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            try {
+                int data = pipe.receive();
+                if (data == endData) {
+                    throw new InterruptedException();
+                }
+                System.out.println("[ " + LocalDateTime.now() + " ] Data : " + data);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
