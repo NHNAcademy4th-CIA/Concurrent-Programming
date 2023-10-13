3fbb17b764766f28b6b68a594b7b8185959648e9
masiljangajji <hjk172262@gmail.com>
2023-10-13 15:07:52 +0900

    feat: 데몬 스레드 Solve

---
+++ src/main/java/org/nhnacademy/lsj/section6/TestDaemonThread.java
@@ -0,0 +1,70 @@
+package org.nhnacademy.lsj.section6;
+
+public class TestDaemonThread {
+
+
+    public static class DaemonThread extends Thread {
+        @Override
+        public void run() {
+
+            while (!Thread.interrupted()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("종료됩니다.");
+                }
+                System.out.println("1초에 한번 씩 출력 합니다 Daemon.");
+
+            }
+
+        }
+    }
+
+
+    public static class WorkerThread extends Thread {
+        @Override
+        public void run() {
+
+            DaemonThread daemonThread = new DaemonThread();
+            daemonThread.setDaemon(true);
+
+            daemonThread.start();
+
+            while (!Thread.interrupted()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                    break;
+                }
+
+                System.out.println("1초에 한번 씩 출력 합니다. Main");
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        WorkerThread workerThread = new WorkerThread();
+
+        workerThread.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        workerThread.interrupt();
+
+        System.out.println("Main Thread terminated");
+
+        // Daemon 은 main이 끝나니 알아서 종료.
+
+    }
+
+
+}
