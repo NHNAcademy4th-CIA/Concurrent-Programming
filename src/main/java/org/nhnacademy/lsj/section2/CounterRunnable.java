5d900069d07180976d0cc729e0fa5dc72feb150c
masiljangajji <hjk172262@gmail.com>
2023-10-14 20:41:56 +0900

    refactor: 자바 doc 추가 및 문제 세분화 정리

--- src/main/java/org/nhnacademy/lsj/section2/CounterRunnable.java
+++ src/main/java/org/nhnacademy/lsj/section2/CounterRunnable.java
@@ -6,9 +6,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Runnable 인터페이스 구현.
  * 스레드 생성은 앞서 소개한 클래스 상속외에 Runnable 인터페이스를 통해서도 가능하다.
- * <p>
  * Thread 클래스의 상속도를 보면 Thread 클래스도 Runnable 인터페이스를 구현하여 정의되었음을 알 수 있다.
- * <p>
  * Runnable 인터페이스를 이용한 스레드 클래스 생성을 확인한다.
  */
 public class CounterRunnable implements Runnable {
@@ -29,6 +27,9 @@ public class CounterRunnable implements Runnable {
         this.thread.start();
     }
 
+    /**
+     * 1초마다 count 출력.
+     */
     @Override
     public void run() {
 
@@ -46,6 +47,9 @@ public class CounterRunnable implements Runnable {
     }
 
 
+    /**
+     * 연습 2-1. CounterRunnable 클래스를 만들어 동작 확인하기.
+     */
     public static void problem1() { // 기존과 같은 결과
         CounterRunnable counterRunnable = new CounterRunnable("CounterRunnable");
 
@@ -54,6 +58,9 @@ public class CounterRunnable implements Runnable {
 
     }
 
+    /**
+     * 연습 2-2. CounterRunnable 클래스의 인스턴스를 동시에 2개 실행해 확인하기.
+     */
     public static void problem2() {
 
         CounterRunnable counterRunnable = new CounterRunnable("CounterRunnable1");
@@ -61,8 +68,16 @@ public class CounterRunnable implements Runnable {
 
         Thread thread = new Thread(counterRunnable);
         Thread thread2 = new Thread(counterRunnable2);
-        thread.start();
+        thread.start(); // multi thread 잘 작동함.
         thread2.start();
+
+
+    }
+
+    /**
+     * 연습 2-3. 같은 공유자원을 쓰는 경우 count 확인 .
+     */
+    public static void problem3() {
 
         // 만약 같은 CounterRunnable 을 갖고 실행시킨다면??
 
@@ -72,26 +87,6 @@ public class CounterRunnable implements Runnable {
 
         threadDulpicated.start(); // 이 경우에는 공유자원이 같기 떄문에 , 1씩 증가하지 않음
         threadDulpicated2.start();
-
-
-    }
-
-    /**
-     * CounterRunnable 클래스는 Runnable 인터페이스를 이용함으로 별도의 Thread 클래스 인스턴스의 도움이 필요하고
-     * 두개의 인스턴스 관리를 해줘야 하는 문제가 생긴다.
-     * <p>
-     * 이를 해결하기 위해 사용되는 방법으로 Runnable 인터페이스를 구현하는 클래스 내부에서
-     * Thread 클래스 인스턴스를 필드로 관리할 수 있다.
-     */
-    public static void problem3() {
-
-        CounterRunnable counterRunnable = new CounterRunnable("counterRunnable");
-        CounterRunnable counterRunnable2 = new CounterRunnable("counterRunnable2");
-
-        counterRunnable.start();
-        counterRunnable2.start();
-
-        logger.info("이건 multi thread입니다.");
 
     }
 
